cc82e4cf06f7ffa0672ae808487e3d216f68579c
Xiaotong Wu <wxt199003@gmail.com>
2020-02-18 10:27:18 +0800

    implement cms_ldp and fcs_ldp

--- algs/cms_ldp.py
+++ algs/cms_ldp.py
@@ -26,6 +26,7 @@ class CMSLDP(SketchLDP):
         self.hash_num = math.ceil(math.log2(1 / self.confidence))
         self.generate_hash_index(self.hash_num)
         self.get_parameters_of_hash()
+        self.sketch = np.zeros([self.hash_num, self.bit_len])
 
     def generate_hash_index(self, hash_num):
         hash_index = []
@@ -41,19 +42,49 @@ class CMSLDP(SketchLDP):
         for i in range(self.hash_num):
             self.hash_parameters.append(parameters[self.hash_index[i]])
 
-    def get_frequency_estimation(self):
-        for i in range(self.data_len):
-            pass
-
     def client_cms_ldp(self, element):
         sub_privacy = self.privacy/self.hash_num
         values = np.zeros([self.hash_num, self.bit_len])
         for i in range(self.hash_num):
-
-            pos = hash_functions.cw_trick_2()
+            para = self.hash_parameters[i]
+            pos = hash_functions.cw_trick_2(
+                element, para[0], para[1]) % self.bit_len
+            y = self.random_generator(sub_privacy, pos)
+            values[i][y] = 1
+        return values
 
     def sketch_cms_ldp(self):
-        pass
+        for i in range(self.data_len):
+            values = self.client_cms_ldp(self.data[i])
+            self.sketch += values
+        sub_privacy = self.privacy / self.hash_num
+        e_privacy = math.exp(sub_privacy)
+        p_positive = e_privacy / (e_privacy + self.bit_len - 1)
+        p_negative = 1 / (e_privacy + self.bit_len - 1)
+        q = p_positive/self.bit_len + \
+            (self.bit_len - 1) * p_negative / self.bit_len
+        self.sketch = (self.sketch - self.data_len*q)/(p_positive - q)
 
-    def server_cms_ldp(self):
-        pass
+    def server_cms_ldp(self, element):
+        f = list()
+        for i in range(self.hash_num):
+            para = self.hash_parameters[i]
+            pos = hash_functions.cw_trick_2(element, para[0], para[1])
+            pos = pos % self.bit_len
+            f.append(self.sketch[i][pos])
+        return min(f)
+
+    def random_generator(self, sub_privacy, pos):
+        e_privacy = math.exp(sub_privacy)
+        p_positive = e_privacy/(e_privacy+self.bit_len-1)
+        p_negative = 1/(e_privacy+self.bit_len-1)
+
+        p = random.uniform(0, 1)
+
+        if p < pos * p_negative:
+            return math.ceil(p/p_negative) - 1
+
+        if p < pos * p_negative + p_positive:
+            return pos
+
+        return pos + math.ceil(p - pos*p_negative - p_positive)
